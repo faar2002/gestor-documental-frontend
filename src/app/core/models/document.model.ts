5957d59cb4be1ffaabcec844fb7229d84e7d94f0
faar2002 <faar2002@gmail.com>
2026-09-04 21:02:20 -0400

    ajustes para mostrar los documentos de un usuario

--- src/app/core/models/document.model.ts
+++
@@ -1,9 +0,0 @@
-export interface DocumentResponse {
-  id: number;
-  fileName: string;
-  fileType: string;
-  size: number;
-  uploadDate: string;
-  companyId?: number;
-  workGroupId?: number;
-}
--- src/app/features/documents/documents.ts
+++
@@ -1,9 +0,0 @@
-import { Component } from '@angular/core';
-
-@Component({
-  imports: [],
-  selector: 'app-documents',
-  styleUrl: './documents.scss',
-  templateUrl: './documents.html',
-})
-export class DocumentsComponent {}
